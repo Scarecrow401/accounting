b2930ef63d88b3251608178d5eac4ae5ad556784
Scarecrow401 <43365620+Scarecrow401@users.noreply.github.com>
2022-03-13 00:00:12 +0500

    prototype home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,13 +10,26 @@ export class HomeComponent implements OnInit {
     { id: 0, name: 'Образование' },
     { id: 1, name: 'Еда' },
     { id: 2, name: 'Такси' },
-    { id: 3, name: 'Прочее' },
+    { id: 3, name: 'Кредит' },
+    { id: 4, name: 'Прочее' },
   ];
 
-  selectedCategory;
-  cost;
+  items = [
+    { selectedCategory: this.categories[1], cost: 1200 },
+    { selectedCategory: this.categories[0], cost: 25000 },
+    { selectedCategory: this.categories[2], cost: 4215 },
+    { selectedCategory: this.categories[3], cost: 22000 },
+    { selectedCategory: this.categories[4], cost: 12530 },
+  ];
+
+  total: number;
+  balance: number;
+  salary = 80000;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.items.reduce((acc, item) => (this.total = acc += item.cost), 0);
+    this.balance = this.salary - this.total;
+  }
 }
